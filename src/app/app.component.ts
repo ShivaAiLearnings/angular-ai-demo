79fd5caf2551336689096bbf9f738a2d4d1ab098
Sivakumar Reddy <SivakumarYeiaka@hlbb.hongleong.com.my>
2026-08-18 23:08:21 +0800

    fix: intentional type error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: number = "This is a string, not a number!";
+  title = 'angular-ai-demo';
 }
