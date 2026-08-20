eb729365d6656168eb155e79bf1596333d4a774f
ShivaAiLearnings <sivakumarreddyyeiaka@gmail.com>
2026-08-20 16:14:19 +0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 
+interface UserProfile {
+  id: number;
+  username: string; // Required property
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
-interface UserProfile {
-  id: number;
-  username: string; // Required property
-}
 export class AppComponent {
   title: string = "This is a string, not a number!"; 
-  user: UserProfile = { 
-    id: 101 
-  };
+ user: UserProfile = {
++  id: 101,
++  username: 'john_doe'
+ };
 }
