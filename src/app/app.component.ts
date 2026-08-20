b19be46ddddd71fdbeb31df84b74a0f41029e8c8
Sivakumar Reddy <SivakumarYeiaka@hlbb.hongleong.com.my>
2026-08-20 16:09:40 +0800

    test: re-introduce run error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+interface UserProfile {
+  id: number;
+  username: string; // Required property
+}
 export class AppComponent {
   title: string = "This is a string, not a number!"; 
+  user: UserProfile = { 
+    id: 101 
+  };
 }
